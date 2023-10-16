56b7a0f36c3487be95fa80dec47dc2109442d992
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2023-10-16 21:11:41 +0100

    [ADD] helper functions for main.py such as auto scripting on preprocess function

--- src/helperfunctions.py
+++ src/helperfunctions.py
@@ -1,7 +1,15 @@
+from os import listdir
+from os.path import isfile, join
 import numpy as np
+import re
+from datapreprocessing import *
 
-def filepath(s):
-    return 'src/data/' + s
+# For datapreprocessing.py ------------------------------------------
+def getfilepath1(filename):
+    return 'src/raw_data/' + filename
+
+def getfilepath2(folder, filename):
+    return 'D:/Individual Project/' + folder + '/' + filename
 
 def complex_converter(s):
     if 'i' in s:
@@ -27,3 +35,55 @@ def oddnumber(x):
             if y[k] % 2 == 0:
                 y[k] = y[k] + 1
         return y  # Return array
+    
+# For main.py ------------------------------------------
+def getfilenamesfromdir(folder):
+    path = f"D:/Individual Project/{folder}"
+    return [f for f in listdir(path) if isfile(join(path, f))]
+
+def check_consistent_activity(filename):
+    k, xx, yy, z = re.split('P|A|R', filename)
+    if int(k) == int(yy):
+        return True
+    else:
+        # print(f"k: {k}\nxx: {xx}\nyy: {yy}\nz: {z}")
+        print(filename)
+        return False
+
+def are_folders_clear(folders):
+    for folder in folders:
+        print(folder + ":")
+        isclear = True
+        filenames = getfilenamesfromdir(f"D:/Individual Project/{folder}")
+        for filename in filenames:
+            if not check_consistent_activity(filename):
+                isclear = False
+        print("isClear: " + str(isclear))
+
+def store_dataset_by_dir(folders):
+    for folder in folders:
+        activities_dataset = []
+        print(folder + ":")
+        filenames = getfilenamesfromdir(folder)
+        for filename in filenames:
+            data = preprocess(folder, filename, False, False, True)
+            if data is not None:
+                activities_dataset.append(data)
+                print(filename)
+            else:
+                print(f"{filename}: Failed")
+        np.save(f"{folder}.npy", activities_dataset)
+        print("Done Saving!!")
+
+def store_label_by_dir(folders):
+    for folder in folders:
+        activities_label = []
+        print(folder + ":")
+        filenames = getfilenamesfromdir(folder)
+        for filename in filenames:
+            # Only for "6 February 2019 NG Homes Dataset"
+            # if filename in ("1P20A01R1", "1P20A01R2", "1P20A01R3", "1P21A01R1", "1P21A01R2", "1P26A01R1", "1P26A01R2", "1P26A01R3"):
+            #     continue
+            activities_label.append(int(filename[0]))
+        np.save(f"{folder[:-7]}Label.npy", activities_label)
+        print("Done Saving!!")
