9124a597970efd584c726e3167cc71bba0eb886d
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2023-11-14 02:03:35 +0000

    [MODIFY] helperfunctions for participant training

--- src/helperfunctions.py
+++ src/helperfunctions.py
@@ -7,9 +7,9 @@ RAWDATAFOLDER = "D:/Individual Project/Data"
 PREPROCESSEDFOLDER = "D:/Individual Project/Preprocessed Activity Data"
 PREPROCESSEDFOLDER2 = "D:/Individual Project/Preprocessed Participant Data"
 
-def getfilenamesfromdir(folder):
-    path = f"{RAWDATAFOLDER}/{folder}"
-    return [f for f in listdir(path) if isfile(join(path, f))]
+def getfilenamesfromdir(path, folder):
+    fullpath = f"{path}/{folder}"
+    return [f for f in listdir(fullpath) if isfile(join(fullpath, f))]
 
 def check_consistent_activity(filename):
     k, _, yy, _ = re.split('P|A|R', filename)
@@ -23,18 +23,30 @@ def are_folders_clear(folders):
     for folder in folders:
         print(folder + ":")
         isclear = True
-        filenames = getfilenamesfromdir(f"{RAWDATAFOLDER}/{folder}")
+        filenames = getfilenamesfromdir(RAWDATAFOLDER, folder)
         for filename in filenames:
             if not check_consistent_activity(filename):
                 isclear = False
         print("isClear: " + str(isclear))
+
+def check_participants_id(folders):
+    for folder in folders:
+        print(f"{folder}:")
+        hashmap = {}
+        filenames = getfilenamesfromdir(PREPROCESSEDFOLDER2, folder)
+        for filename in filenames:
+            pid = re.split("P|A", filename)[1]
+            if pid not in hashmap:
+                hashmap[pid] = None
+        print([k for k in hashmap.keys()])
+        print("\n")
 
 # For Preprocessed Data ------------------------------------------------------
 # Store dataset to folder
 # folders = ['1 December 2017 Dataset', '2 March 2017 Dataset', '3 June 2017 Dataset', '4 July 2018 Dataset', '5 February 2019 UoG Dataset', '6 February 2019 NG Homes Dataset', '7 March 2019 West Cumbria Dataset']
 # for folder in folders:
 #     print(folder + ":")
-#     filenames = getfilenamesfromdir(folder)
+#     filenames = getfilenamesfromdir(RAWDATAFOLDER, folder)
 #     for filename in filenames:
 #         data = preprocess(folder, filename, False, False, True, True)
 #         np.save(f"{PREPROCESSEDFOLDER}/{folder} Normalized/{filename[:-4]} Normalized.npy", data)
@@ -43,18 +55,18 @@ def are_folders_clear(folders):
 
 def vstack_preprocessdata(folder, name):
     datalist = []
-    filenames = getfilenamesfromdir(folder)
+    filenames = getfilenamesfromdir(PREPROCESSEDFOLDER2, folder)
     for filename in filenames:
         # Repetition == 1
-        # if int(re.split("R| ", filename)[1].split('.')[0]) == 1:
+        if int(re.split("R| ", filename)[1].split('.')[0]) == 1:
 
         # Repetition == 2
-        if int(re.split("R| ", filename)[1].split('.')[0]) == 2:
+        # if int(re.split("R| ", filename)[1].split('.')[0]) == 2:
 
-        # Repetition == 1 & 3
-        # if int(re.split("R| ", filename)[1].split('.')[0]) in (1, 3):
+        # Repetition == 2 & 3
+        # if int(re.split("R| ", filename)[1].split('.')[0]) in (2, 3):
             print(filename + ": ")
-            datalist.append(np.load(f"{PREPROCESSEDFOLDER}/{folder} Normalized/{filename[:-4]} Normalized.npy"))
+            datalist.append(np.load(f"{PREPROCESSEDFOLDER}/{folder}/{filename}"))
     print("Storing...")
     print(np.array(datalist).shape)
     # Saved in activity data -----------------
@@ -68,11 +80,9 @@ def vstack_datasets(folders, name):
     for folder in folders:
         print(folder + ": ")
         if vstack_datasets is None:
-            # vstack_datasets = np.load(f"{PREPROCESSEDFOLDER}/{folder}.npy")
-            vstack_datasets = np.load(f"{PREPROCESSEDFOLDER2}/{folder} R2.npy")
+            vstack_datasets = np.load(f"{PREPROCESSEDFOLDER2}/{folder} R1.npy")
         else:
-            # dataset = np.load(f"{PREPROCESSEDFOLDER}/{folder}.npy")
-            dataset = np.load(f"{PREPROCESSEDFOLDER2}/{folder} R2.npy")
+            dataset = np.load(f"{PREPROCESSEDFOLDER2}/{folder} R1.npy")
             vstack_datasets = np.vstack((vstack_datasets, dataset))
     print("Storing...")
     print(vstack_datasets.shape)
@@ -86,7 +96,7 @@ def store_activity_labels(folders):
     for folder in folders:
         activities_label = []
         print(folder + ":")
-        filenames = getfilenamesfromdir(folder)
+        filenames = getfilenamesfromdir(RAWDATAFOLDER, folder)
         for filename in filenames:
             # Repetition == 1
             # if int(re.split("R| ", filename)[1].split('.')[0]) == 1:
@@ -95,23 +105,22 @@ def store_activity_labels(folders):
         np.save(f"{PREPROCESSEDFOLDER}/{folder[:-8]} Label.npy", np.array(activities_label))
         print("Done Saving!!")
 
-def store_participant_labels(folders):
+def store_participant_labels(folders, name):
     dataset1 = {"36":0, "37":1, "38":2, "39":3, "40":4, "41":5, "42":6, "43":7, "44":8, "45":9, "46":10, "47":11, "50":12, "51":13, "52":14, "53":15, "54":16, "55":17, "56":18, "57":19}
-    dataset2 = {"03":20, "10":21, "11":22, "12":23}
-    dataset3 = {"14":24, "28":25, "29":26, "30":27, "31":28, "32":29, "33":30, "34":31, "35":32}
-    dataset4 = {"57":33, "58":34, "59":35, "60":36, "61":37, "62":38, "63":39, "64":40, "65":41, "66":42, "67":43, "68":44, "69":45, "70":46, "71":47, "72":48}
-    dataset5 = {"01":49, "02":50, "03":51, "04":52, "05":53, "06":54, "07":55, "08":56, "09":57, "10":58, "11":59, "12":60, "13":61, "14":62, "15":63, "16":64, "17":65}
-    dataset6 = {"08":66, "18":67, "19":68, "20":69, "21":70, "22":71, "23":72, "24":73, "25":74, "26":75, "27":76, "28":77, "29":78, "30":79, "31":80, "32":81, "33":82, "34":83, "35":84, "36":85}
-    dataset7 = {"37":86, "38":87, "39":88, "40":89, "41":90, "42":91, "43":92, "44":93, "45":94, "46":95, "47":96, "48":97, "49":98, "50":99, "51":100, "52":101, "53": 102, "54": 103, "55": 104, "56": 105}
+    dataset3 = {"14":20, "28":21, "29":22, "30":23, "31":24, "32":25, "33":26, "34":27}
+    dataset4 = {"57":28, "58":29, "59":30, "60":31, "61":32, "62":33, "64":34, "65":35, "66":36, "67":37, "68":38, "69":39, "70":40, "71":41, "72":42}
+    dataset5 = {"01":43, "02":44, "03":45, "04":46, "05":47, "06":48, "07":49, "08":50, "09":51, "10":52, "11":53, "12":54, "13":55, "14":56, "15":57, "16":58, "17":59}
+    dataset6 = {"08":60}
     for folder in folders:
         activities_label = []
         print(folder + ":")
-        filenames = getfilenamesfromdir(folder)
+        filenames = getfilenamesfromdir(PREPROCESSEDFOLDER2, folder)
         for filename in filenames:
-            # Repetition == 2
-            if int(re.split("R| ", filename)[1].split('.')[0]) == 2:
-                activities_label.append(dataset7[re.split("P|A", filename)[1]])
-        np.save(f"{PREPROCESSEDFOLDER2}/{folder[:-8]} Label R2.npy", np.array(activities_label))
+            # Repetition == 2 & 3
+            if int(re.split("R| ", filename)[1].split('.')[0]) in (2, 3):
+                activities_label.append(dataset1[re.split("P|A", filename)[1]])
+        print(np.array(activities_label).shape)
+        np.save(f"{PREPROCESSEDFOLDER2}/{name}.npy", np.array(activities_label))
         print("Done Saving!!")
 
 def vstack_labels(folders, name):
@@ -120,11 +129,10 @@ def vstack_labels(folders, name):
         print(folder + ": ")
         if vstacklabel is None:
             # vstacklabel = np.load(f"{PREPROCESSEDFOLDER}/{folder[:-8]} Label R1.npy")
-            # vstacklabel = np.load(f"{PREPROCESSEDFOLDER}/{folder[:-8]} Label.npy")
-            vstacklabel = np.load(f"{PREPROCESSEDFOLDER2}/{folder[:-8]} Label R2.npy")
+            vstacklabel = np.load(f"{PREPROCESSEDFOLDER2}/{folder[:-8]} Label R1.npy")
         else:
             # label = np.load(f"{PREPROCESSEDFOLDER}/{folder[:-8]} Label R1.npy")
-            label = np.load(f"{PREPROCESSEDFOLDER2}/{folder[:-8]} Label R2.npy")
+            label = np.load(f"{PREPROCESSEDFOLDER2}/{folder[:-8]} Label R1.npy")
             vstacklabel = np.concatenate((vstacklabel, label))
     print("Storing...")
     print(vstacklabel.shape)
