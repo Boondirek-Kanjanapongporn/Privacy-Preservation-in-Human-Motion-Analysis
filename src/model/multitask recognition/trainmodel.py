0d89ffecbc7a5551c7eda450de0020b8a5e0af3e
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2023-12-14 01:10:19 +0700

    [ADD] alternatives for loading data and cumtom training loop for mixup data augmentation and normal model training

--- src/model/multitask recognition/trainmodel.py
+++ src/model/multitask recognition/trainmodel.py
@@ -3,58 +3,72 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.utils import shuffle
 
-PREPROCESSEDFOLDER = "D:/Individual Project/Preprocessed Participant Data"
+PREPROCESSEDFOLDER = "D:/Individual Project/Preprocessed Multitask Data"
 
 # Alternative 1 --------------------------------------------------------------
-TRAINDATASET = "trainDataset.npy"
-TRAINPARTICIPANTLABEL = "trainlabel.npy"
-VALIDATIONDATASET = "validationDataset.npy"
-VALIDATIONLABEL = "validationlabel.npy"
+TRAINDATASET = "trainDataset30.npy"
+TRAINLABEL_ACTIVITY = "trainLabel_activity30.npy"
+TRAINLABEL_PARTICIPANT = "trainLabel_participant30.npy"
+VALIDATIONDATASET = "validationDataset30.npy"
+VALIDATIONLABEL_ACTIVITY = "validationLabel_activity30.npy"
+VALIDATIONLABEL_PARTICIPANT = "validationLabel_participant30.npy"
 
 # Load data and labels
 train_data = np.load(f"{PREPROCESSEDFOLDER}/{TRAINDATASET}")
-train_participant_labels = np.load(f"{PREPROCESSEDFOLDER}/{TRAINPARTICIPANTLABEL}")
+train_labels_activity = np.load(f"{PREPROCESSEDFOLDER}/{TRAINLABEL_ACTIVITY}")
+train_labels_participant = np.load(f"{PREPROCESSEDFOLDER}/{TRAINLABEL_PARTICIPANT}")
 validate_data = np.load(f"{PREPROCESSEDFOLDER}/{VALIDATIONDATASET}")
-validate_participant_labels = np.load(f"{PREPROCESSEDFOLDER}/{VALIDATIONLABEL}")
+validate_labels_activity = np.load(f"{PREPROCESSEDFOLDER}/{VALIDATIONLABEL_ACTIVITY}")
+validate_labels_participant = np.load(f"{PREPROCESSEDFOLDER}/{VALIDATIONLABEL_PARTICIPANT}")
 
-# Data Augment on Train data
-train_data_fliplr = []
-train_data_flipud = []
-train_data_fliplr_flipud = []
-for d in train_data:
-    train_data_fliplr.append(np.fliplr(d))
-    train_data_flipud.append(np.flipud(d))
-    train_data_fliplr_flipud.append(np.fliplr(np.flipud(d)))
-train_data_fliplr = np.array(train_data_fliplr)
-train_data_flipud = np.array(train_data_flipud)
-train_data_fliplr_flipud = np.array(train_data_fliplr_flipud)
-train_data = np.concatenate((train_data, train_data_fliplr))
-train_data = np.concatenate((train_data, train_data_flipud))
-train_data = np.concatenate((train_data, train_data_fliplr_flipud))
+# # Data Augment on Train data
+# train_data_fliplr = []
+# train_data_flipud = []
+# train_data_fliplr_flipud = []
+# for d in train_data:
+#     train_data_fliplr.append(np.fliplr(d))
+#     train_data_flipud.append(np.flipud(d))
+#     train_data_fliplr_flipud.append(np.fliplr(np.flipud(d)))
+# train_data_fliplr = np.array(train_data_fliplr)
+# train_data_flipud = np.array(train_data_flipud)
+# train_data_fliplr_flipud = np.array(train_data_fliplr_flipud)
+# train_data = np.concatenate((train_data, train_data_fliplr))
+# train_data = np.concatenate((train_data, train_data_flipud))
+# train_data = np.concatenate((train_data, train_data_fliplr_flipud))
 
-# Extend Train label
-label_data = np.copy(train_participant_labels)
-train_participant_labels = np.concatenate((train_participant_labels, label_data))
-train_participant_labels = np.concatenate((train_participant_labels, label_data))
-train_participant_labels = np.concatenate((train_participant_labels, label_data))
+# # Extend Train label Activity
+# label_data_activity = np.copy(train_labels_activity)
+# train_labels_activity = np.concatenate((train_labels_activity, label_data_activity))
+# train_labels_activity = np.concatenate((train_labels_activity, label_data_activity))
+# train_labels_activity = np.concatenate((train_labels_activity, label_data_activity))
+
+# # Extend Train label Participant
+# label_data_participant = np.copy(train_labels_participant)
+# train_labels_participant = np.concatenate((train_labels_participant, label_data_participant))
+# train_labels_participant = np.concatenate((train_labels_participant, label_data_participant))
+# train_labels_participant = np.concatenate((train_labels_participant, label_data_participant))
 
 # Shuffle data and labels
 train_indices = np.arange(len(train_data))
 np.random.shuffle(train_indices)
 x_train = train_data[train_indices]
-y_participant_train = train_participant_labels[train_indices]
+y_train_activity = train_labels_activity[train_indices]
+y_train_participant = train_labels_participant[train_indices]
 
 validate_indices = np.arange(len(validate_data))
 np.random.shuffle(validate_indices)
 x_validate = validate_data[validate_indices]
-y_participant_validate = validate_participant_labels[validate_indices]
+y_validate_activity = validate_labels_activity[validate_indices]
+y_validate_participant = validate_labels_participant[validate_indices]
 # ----------------------------------------------------------------------------
 
 # Print Data shape
 print("x_train:", x_train.shape)
-print("y_participant_train:", y_participant_train.shape)
+print("y_train_activity:", y_train_activity.shape)
+print("y_train_participant:", y_train_participant.shape)
 print("x_validate:", x_validate.shape)
-print("y_participant_validate:", y_participant_validate.shape)
+print("y_validate_activity:", y_validate_activity.shape)
+print("y_validate_participant:", y_validate_participant.shape)
 
 # Save image parameters to the constants that we will use later for data re-shaping and for model traning.
 (_, WIDTH, HEIGHT) = x_train.shape
@@ -85,13 +99,13 @@ print("Building Model:")
 input_layer = tf.keras.layers.Input(shape=(WIDTH, HEIGHT, CHANNELS))
 
 # Shared convolutional layers
-conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(input_layer)
+conv1 = tf.keras.layers.Conv2D(32, 3, activation='relu')(input_layer)
 pool1 = tf.keras.layers.MaxPooling2D((2, 2))(conv1)
-conv2 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(pool1)
+conv2 = tf.keras.layers.Conv2D(64, 3, activation='relu')(pool1)
 pool2 = tf.keras.layers.MaxPooling2D((2, 2))(conv2)
-conv3 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu')(pool2)
+conv3 = tf.keras.layers.Conv2D(128, 3, activation='relu')(pool2)
 pool3 = tf.keras.layers.MaxPooling2D((2, 2))(conv3)
-conv4 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu')(pool3)
+conv4 = tf.keras.layers.Conv2D(256, 3, activation='relu')(pool3)
 pool4 = tf.keras.layers.MaxPooling2D((2, 2))(conv4)
 flatten = tf.keras.layers.Flatten()(pool4)
 
@@ -106,78 +120,210 @@ activity_output = tf.keras.layers.Dense(6, activation='softmax', name='activity_
 participant_dense1 = tf.keras.layers.Dense(128, activation='relu')(flatten)
 participant_dropout1 = tf.keras.layers.Dropout(0.25)(participant_dense1)
 participant_dense2 = tf.keras.layers.Dense(64, activation='relu')(participant_dropout1)
-participant_dropout2 = tf.keras.layers.Dropout(0.5)(participant_dense2)
-participant_output = tf.keras.layers.Dense(61, activation='softmax', name='participant_output')(participant_dropout2)
+participant_dropout2 = tf.keras.layers.Dropout(0.4)(participant_dense2)
+participant_output = tf.keras.layers.Dense(30, activation='softmax', name='participant_output')(participant_dropout2)
 
 # Create a multi-task model
-multi_task_model = tf.keras.models.Model(inputs=input_layer, outputs=[activity_output, participant_output])
+model = tf.keras.models.Model(inputs=input_layer, outputs=[activity_output, participant_output])
+print(model.summary())
 
 # Compile the model with appropriate loss functions and metrics for each task
-multi_task_model.compile(optimizer='adam',
-                         loss={'activity_output': 'categorical_crossentropy', 'participant_output': 'categorical_crossentropy'},
-                         loss_weights={'activity_output': 1.0, 'participant_output': 1.0},
-                         metrics={'activity_output': 'accuracy', 'participant_output': 'accuracy'})
-
-print(multi_task_model.summary())
-
 print("Compile Model:")
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.00015)
-multi_task_model.compile(
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0003)
+model.compile(
     optimizer=optimizer,
     loss={'activity_output': 'categorical_crossentropy', 'participant_output': 'categorical_crossentropy'},
-    loss_weights={'activity_output': 1.0, 'participant_output': 1.0},
+    loss_weights={'activity_output': 1.0, 'participant_output': 8.0},
     metrics={'activity_output': 'accuracy', 'participant_output': 'accuracy'}
 )
 
 print("Train Model:")
-y_train = tf.keras.utils.to_categorical(y_train, num_classes=61)
-y_validate = tf.keras.utils.to_categorical(y_validate, num_classes=61)
+# Set categorical
+y_train_activity = tf.keras.utils.to_categorical(y_train_activity, num_classes=6)
+y_validate_activity = tf.keras.utils.to_categorical(y_validate_activity, num_classes=6)
+
+y_train_participant = tf.keras.utils.to_categorical(y_train_participant, num_classes=30)
+y_validate_participant = tf.keras.utils.to_categorical(y_validate_participant, num_classes=30)
 
 # Add early stopping
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
 # Training parameters
 train_epochs = 30
 train_batch_size = 16
 
-# Built-in Training loop -----------------------------------------------------
-# Train model
-training_history = multi_task_model.fit(
-    x_train, 
-    {'activity_output': y_activity_train, 'participant_output': y_participant_train}, 
-    epochs=train_epochs,
-    batch_size=train_batch_size, 
-    validation_data=(x_validate, {'activity_output': y_activity_validate, 'participant_output': y_participant_validate}),
-    callbacks=[early_stopping]
-)
+# Custom Training loop -------------------------------------------------------
+def mixup_data(x, y_activity, y_participant, alpha=0.2):
+    # Applies the Mixup augmentation
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.shape[0]
+    index = np.random.permutation(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    mixed_y_activity = lam * y_activity + (1 - lam) * y_activity[index, :]
+    mixed_y_participant = lam * y_participant + (1 - lam) * y_participant[index, :]
+    return mixed_x, mixed_y_activity, mixed_y_participant
+
+# Create list for storing history footprint
+history_train_loss_activity = []
+history_train_accuracy_activity = []
+history_validation_loss_activity = []
+history_validation_accuracy_activity = []
+history_train_loss_participant = []
+history_train_accuracy_participant = []
+history_validation_loss_participant = []
+history_validation_accuracy_participant = []
+
+# with tf.device('/cpu:0'):
+# Custom training loop with Mixup
+for epoch in range(train_epochs):
+    print('Epoch', epoch + 1, '/', train_epochs)
+    x_train, y_train_activity, y_train_participant = shuffle(x_train, y_train_activity, y_train_participant)  # Shuffle the data in each epoch
+    for i in range(0, x_train.shape[0], train_batch_size):
+        x_batch = x_train[i:i + train_batch_size]
+        y_batch_activity = y_train_activity[i:i + train_batch_size]
+        y_batch_participant = y_train_participant[i:i + train_batch_size]
+
+        # Apply Mixup
+        x_batch, y_batch_activity, y_batch_participant = mixup_data(x_batch, y_batch_activity, y_batch_participant, alpha=0.2)
+
+        # Train on batch
+        model.train_on_batch(x_batch, {'activity_output': y_batch_activity, 'participant_output': y_batch_participant})
+
+    # Validate after each epoch
+    _, activity_loss_train, participant_loss_train, activity_accuracy_train, participant_accuracy_train = model.evaluate(x_train, {'activity_output': y_train_activity, 'participant_output': y_train_participant})
+    print('Training loss activity:', activity_loss_train)
+    print('Training accuracy activity:', activity_accuracy_train)
+    print('Training loss participant:', participant_loss_train)
+    print('Training accuracy participant:', participant_accuracy_train)
+
+    _, activity_loss_validation, participant_loss_validation, activity_accuracy_validation, participant_accuracy_validation = model.evaluate(x_validate, {'activity_output': y_validate_activity, 'participant_output': y_validate_participant})
+    print('Validation loss activity:', activity_loss_validation)
+    print('Validation accuracy activity:', activity_accuracy_validation)
+    print('Validation loss participant:', participant_loss_validation)
+    print('Validation accuracy participant:', participant_accuracy_validation)
+
+    # Store into history
+    history_train_loss_activity.append(activity_loss_train)
+    history_train_accuracy_activity.append(activity_accuracy_train)
+    history_validation_loss_activity.append(activity_loss_validation)
+    history_validation_accuracy_activity.append(activity_accuracy_validation)
+
+    history_train_loss_participant.append(participant_loss_train)
+    history_train_accuracy_participant.append(participant_accuracy_train)
+    history_validation_loss_participant.append(participant_loss_validation)
+    history_validation_accuracy_participant.append(participant_accuracy_validation)
+
+    # Early stopping check
+    if early_stopping.model is not None and early_stopping.stopped_epoch > 0:
+        break
 
 plt.figure()
 plt.xlabel('Epoch Number')
 plt.ylabel('Loss')
-plt.plot(training_history.history['loss'], label='training set')
-plt.plot(training_history.history['val_loss'], label='test set')
+plt.plot(history_train_loss_activity, label='training set')
+plt.plot(history_validation_loss_activity, label='validation set')
 plt.legend()
 plt.show(block=False)
 
 plt.figure()
 plt.xlabel('Epoch Number')
 plt.ylabel('Accuracy')
-plt.plot(training_history.history['accuracy'], label='training set')
-plt.plot(training_history.history['val_accuracy'], label='test set')
+plt.plot(history_train_accuracy_activity, label='training set')
+plt.plot(history_validation_accuracy_activity, label='validation set')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.xlabel('Epoch Number')
+plt.ylabel('Loss')
+plt.plot(history_train_loss_participant, label='training set')
+plt.plot(history_validation_loss_participant, label='validation set')
+plt.legend()
+plt.show(block=False)
+
+plt.figure()
+plt.xlabel('Epoch Number')
+plt.ylabel('Accuracy')
+plt.plot(history_train_accuracy_participant, label='training set')
+plt.plot(history_validation_accuracy_participant, label='validation set')
 plt.legend()
 plt.show()
 # ----------------------------------------------------------------------------
 
-# Training Loss
-train_loss, train_accuracy = model.evaluate(x_train, y_train)
-print('Training loss: ', train_loss)
-print('Training accuracy: ', train_accuracy)
+# Built-in Training loop -----------------------------------------------------
+# # Train model
+# training_history = model.fit(
+#     x_train, 
+#     {'activity_output': y_train_activity, 'participant_output': y_train_participant}, 
+#     epochs=train_epochs,
+#     batch_size=train_batch_size, 
+#     validation_data=(x_validate, {'activity_output': y_validate_activity, 'participant_output': y_validate_participant}),
+#     callbacks=[early_stopping]
+# )
 
-# Validation Loss
-validation_loss, validation_accuracy = model.evaluate(x_validate, y_validate)
-print('Validation loss: ', validation_loss)
-print('Validation accuracy: ', validation_accuracy)
+# # Plot loss for activity recognition
+# plt.figure()
+# plt.title('Activity Losses')
+# plt.xlabel('Epoch Number')
+# plt.ylabel('Activity Loss')
+# plt.plot(training_history.history['activity_output_loss'], label='training set')
+# plt.plot(training_history.history['val_activity_output_loss'], label='validation set')
+# plt.legend()
+# plt.show(block=False)
 
+# # Plot accuracy for activity recognition
+# plt.figure()
+# plt.title('Activity Accuracies')
+# plt.xlabel('Epoch Number')
+# plt.ylabel('Activity Accuracy')
+# plt.plot(training_history.history['activity_output_accuracy'], label='training set')
+# plt.plot(training_history.history['val_activity_output_accuracy'], label='validation set')
+# plt.legend()
+# plt.show()
+
+# # Plot loss for participant recognition
+# plt.figure()
+# plt.title('Participant Losses')
+# plt.xlabel('Epoch Number')
+# plt.ylabel('Participant Loss')
+# plt.plot(training_history.history['participant_output_loss'], label='training set')
+# plt.plot(training_history.history['val_participant_output_loss'], label='validation set')
+# plt.legend()
+# plt.show(block=False)
+
+# # Plot accuracy for participant recognition
+# plt.figure()
+# plt.title('Participant Accuracies')
+# plt.xlabel('Epoch Number')
+# plt.ylabel('Participant Accuracy')
+# plt.plot(training_history.history['participant_output_accuracy'], label='training set')
+# plt.plot(training_history.history['val_participant_output_accuracy'], label='validation set')
+# plt.legend()
+# plt.show()
+# ----------------------------------------------------------------------------
 # Save Model
-model_name = 'participant_recognition_cnn.h5'
+model_name = 'multitask_recognition_cnn.h5'
 model.save(model_name, save_format='h5')
+
+# Evaluate results
+_, activity_loss_train, participant_loss_train, activity_accuracy_train, participant_accuracy_train = model.evaluate(x_train, {'activity_output': y_train_activity, 'participant_output': y_train_participant})
+_, activity_loss_validation, participant_loss_validation, activity_accuracy_validation, participant_accuracy_validation = model.evaluate(x_validate, {'activity_output': y_validate_activity, 'participant_output': y_validate_participant})
+
+# Evaluate for Train dataset
+print('Activity Recognition:')
+print('Activity Training Loss: ', activity_loss_train)
+print('Activity Training Accuracy: ', activity_accuracy_train)
+print('Activity Validation Loss: ', activity_loss_validation)
+print('Activity Validation Accuracy', activity_accuracy_validation)
+
+# Evaluate for Participant
+print('\nParticipant Recognition:')
+print('Participant Training Loss: ', participant_loss_train)
+print('Participant Training Accuracy: ', participant_accuracy_train)
+print('Participant Validation Loss: ', participant_loss_validation)
+print('Participant Validation Accuracy', participant_accuracy_validation)
